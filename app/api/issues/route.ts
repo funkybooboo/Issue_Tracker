0b62045f8663e9b4567dca1d0d3532543b0194c4
Nate Stott <nate.stott@pm.me>
2024-06-08 12:44:46 -0600

    handle erros

--- app/api/issues/route.ts
+++ app/api/issues/route.ts
@@ -4,8 +4,8 @@ import {z} from "zod";
 import prisma from "@/prisma/client";
 
 const schema = z.object({
-    title: z.string().min(1).max(255),
-    description: z.string().min(1),
+    title: z.string().min(1, "title is required").max(255),
+    description: z.string().min(1, "description is required"),
 });
 
 export async function POST(request: NextRequest) {
@@ -13,7 +13,7 @@ export async function POST(request: NextRequest) {
     console.log(body);
     const validation = schema.safeParse(body);
     if (!validation.success) {
-        return NextResponse.json( validation.error.errors, {status: 400});
+        return NextResponse.json( validation.error.format(), {status: 400});
     }
     const newIssue = await prisma.issue.create({
         data: {
--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -1,11 +1,12 @@
 'use client';
 
-import {Button, TextField} from "@radix-ui/themes";
+import {Button, Callout, TextField} from "@radix-ui/themes";
 import SimpleMDE from "react-simplemde-editor";
 import {useForm, Controller} from "react-hook-form";
 import axios from "axios";
 import "easymde/dist/easymde.min.css";
 import {useRouter} from "next/navigation";
+import {useState} from "react";
 
 interface IssueForm {
     title: string;
@@ -15,30 +16,33 @@ interface IssueForm {
 const NewIssuePage = () => {
     const router = useRouter();
     const {register, control, handleSubmit} = useForm<IssueForm>();
+    const [error, setError] = useState('');
 
     const onSubmit = async (data: IssueForm) => {
         try {
             await axios.post('/api/issues', data);
             router.push('/issues');
         } catch (error) {
-            console.error(error);
+            setError("An error occurred while creating the issue");
         }
     }
 
     return (
-        <form
-            className={"max-w-xl space-y-3"}
-            onSubmit={handleSubmit((data) => onSubmit(data))}
-        >
-            <TextField.Root placeholder={"Title"} {...register('title')}/>
-            <Controller
-                name={"description"}
-                control={control}
-                render={({field}) => <SimpleMDE placeholder={"description"} {...field}/>}
-            />
-            <Button>Submit New Issue</Button>
-        </form>
-
+        <div className={"max-w-xl"}>
+            {error && <Callout.Root color={'red'} className={"mb-5"}><Callout.Text>{error}</Callout.Text></Callout.Root>}
+            <form
+                className={"space-y-3"}
+                onSubmit={handleSubmit((data) => onSubmit(data))}
+            >
+                <TextField.Root placeholder={"Title"} {...register('title')}/>
+                <Controller
+                    name={"description"}
+                    control={control}
+                    render={({field}) => <SimpleMDE placeholder={"description"} {...field}/>}
+                />
+                <Button>Submit New Issue</Button>
+            </form>
+        </div>
     );
 }
 
