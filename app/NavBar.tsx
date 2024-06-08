905d6a64f56545c78be73c1979ca2ee49020c283
Nate Stott <nate.stott@pm.me>
2024-06-08 11:02:55 -0600

    style NavBar

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -1,5 +1,9 @@
+'use client';
+
 import Link from "next/link";
 import { FaBug } from "react-icons/fa";
+import {usePathname} from "next/navigation";
+import classnames from "classnames";
 
 interface Links {
     label: string;
@@ -7,6 +11,9 @@ interface Links {
 }
 
 const NavBar = () => {
+
+    const currentPath = usePathname();
+
     const links: Links[] = [
         { label: "Dashboard", href: "/" },
         { label: "Issues", href: "/issues" },
@@ -16,7 +23,15 @@ const NavBar = () => {
             <Link href={"/"}><FaBug/></Link>
             <ul className={"flex space-x-6"}>
                 {links.map((link) => (
-                    <Link key={link.href} className={"text-zinc-500 hover:text-zinc-800 transition-colors"} href={link.href}>
+                    <Link
+                        key={link.href}
+                        className={classnames({
+                            "text-zinc-900": currentPath === link.href,
+                            "text-zinc-500": currentPath !== link.href,
+                            "hover:text-zinc-800 transition-colors": true,
+                        })}
+                        href={link.href}
+                    >
                         {link.label}
                     </Link>
                 ))}
---
+++ app/issues/page.tsx
@@ -0,0 +1,10 @@
+
+const IssuesPage = () => {
+    return (
+        <div>
+        <h1>Issues</h1>
+        </div>
+    );
+}
+
+export default IssuesPage;
