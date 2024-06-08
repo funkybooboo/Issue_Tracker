eac74fc57a6793b048f60930664d7f6464577bc1
Nate Stott <nate.stott@pm.me>
2024-06-08 13:51:27 -0600

    add loading skelitons

--- app/issues/loading.tsx
+++ app/issues/loading.tsx
@@ -1,11 +1,11 @@
-import {Table} from '@radix-ui/themes';
-import prisma from "@/prisma/client";
-import IssueStatusBadge from "@/app/components/IssueStatusBadge";
+import {Table} from "@radix-ui/themes";
+import Skeleton from 'react-loading-skeleton'
+import 'react-loading-skeleton/dist/skeleton.css'
 import IssueActions from "@/app/issues/IssueActions";
 
-const IssuesPage = async () => {
+const LoadingIssuesPage = () => {
 
-    const issues = await prisma.issue.findMany();
+    const issues = [1, 2, 3, 4, 5];
 
     return (
         <div>
@@ -20,19 +20,19 @@ const IssuesPage = async () => {
                 </Table.Header>
                 <Table.Body>
                     {issues.map((issue) => (
-                        <Table.Row key={issue.id}>
+                        <Table.Row key={issue}>
                             <Table.Cell>
-                                {issue.title}
-                                <div className={"block md:hidden"}><IssueStatusBadge status={issue.status}/></div>
+                                <Skeleton/>
+                                <div className={"block md:hidden"}><Skeleton/></div>
                             </Table.Cell>
-                            <Table.Cell className={"hidden md:table-cell"}> <IssueStatusBadge status={issue.status}/> </Table.Cell>
-                            <Table.Cell className={"hidden md:table-cell"}>{issue.createdAt.toDateString()}</Table.Cell>
+                            <Table.Cell className={"hidden md:table-cell"}><Skeleton/></Table.Cell>
+                            <Table.Cell className={"hidden md:table-cell"}><Skeleton/></Table.Cell>
                         </Table.Row>
                     ))}
                 </Table.Body>
             </Table.Root>
         </div>
-    );
+    )
 }
 
-export default IssuesPage;
+export default LoadingIssuesPage;
