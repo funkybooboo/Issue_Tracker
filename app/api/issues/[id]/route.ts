3592bf0d6d69c3cca9c35bfea9ebd671ca9ee402
Nate Stott <nate.stott@pm.me>
2024-06-15 21:40:44 -0600

    add api to delete an issue

--- app/api/issues/[id]/route.ts
+++ app/api/issues/[id]/route.ts
@@ -32,3 +32,23 @@ export async function PATCH(
     });
     return NextResponse.json(updatedIssue, {status: 200});
 }
+
+export async function DELETE(
+    request: NextRequest,
+    {params}: { params: { id: string; } }
+) {
+    const issue = await prisma.issue.findUnique({
+        where: {
+            id: parseInt(params.id)
+        }
+    });
+    if (!issue) {
+        return NextResponse.json({error: "Invalid Issue"}, {status: 404});
+    }
+    await prisma.issue.delete({
+        where: {
+            id: parseInt(params.id)
+        }
+    });
+    return NextResponse.json({}, {status: 200});
+}
